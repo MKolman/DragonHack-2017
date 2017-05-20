6f7e8540e8f2b13a23332629e960f4b90af645c4
goska <goska.science@gmail.com>
2017-05-21 00:30:09 +0200

    Telo tested and fixed. Without comms with server.

--- telo2.0.py
+++ telo2.0.py
@@ -15,26 +15,27 @@ ALL_PINS = [LEFT_FORWARD_PIN, LEFT_BACKWARD_PIN, RIGHT_FORWARD_PIN, RIGHT_BACKWA
 
 class Wheel(object):
     def __init__(self, front, back):
-	    # front and back are pin numbers
+        # front and back are pin numbers
         # Save both pins
-		GPIO.setup(front, GPIO.OUT)
+        GPIO.setup(front, GPIO.OUT)
         self.front = GPIO.PWM(front, PWM_FREQ)
         self.front.start(0)
-		GPIO.setup(back, GPIO.OUT)
+        GPIO.setup(back, GPIO.OUT)
         self.back = GPIO.PWM(back, PWM_FREQ)
         self.back.start(0)
 
-	def go(self, value):
-	    if value < 0:
-		    self.front.ChangeDutyCycle(0)
-			self.back.ChangeDutyCycle(-value)
-		else:
-		    self.front.ChangeDutyCycle(value)
-			self.back.ChangeDutyCycle(0)
+    def go(self, value):
+        print('value', value)
+        if value < 0:
+            self.front.ChangeDutyCycle(0)
+            self.back.ChangeDutyCycle(-value)
+        else:
+            self.front.ChangeDutyCycle(value)
+            self.back.ChangeDutyCycle(0)
 
     def stop(self):
         self.front.ChangeDutyCycle(0)
-		self.back.ChangeDutyCycle(0)
+        self.back.ChangeDutyCycle(0)
 
 
 class Car(object):
@@ -44,18 +45,18 @@ class Car(object):
 
     def move(self, left, right):
         self.left.go(left)
-		self.right.go(right)
+        self.right.go(right)
 
 try:
     pp3 = Car(Wheel(LEFT_FORWARD_PIN, LEFT_BACKWARD_PIN), Wheel(RIGHT_FORWARD_PIN, RIGHT_BACKWARD_PIN))
-    for i in range(0, 101, 10):
-        #r = requests.get("http://pp3.tech/server/direction")
-		#tmp = r.text.strip().split()
+    for i in range(-100, 101, 10):
+        #r = requests.get("http://pp3.tech/server/direction
+        #tmp = r.text.strip().split()
         #left, right = map(int, tmp)
-		right = 0
-		left = i
+        right = 0
+        left = i
+        print(i)
         pp3.move(left, right)
         time.sleep(1)
 finally:
-    
     GPIO.cleanup()
